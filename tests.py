9abffb3246d26034fefec2a1fa1d17a65f3442fc
Diego N. Santosuosso <78157706+dnsantosuosso@users.noreply.github.com>
2021-02-09 11:41:17 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,17 +4,6 @@
 Department of Computer Science,
 University of Toronto
 
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-Authors: David Liu, Diane Horton and Sophia Huynh
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2020 David Liu, Diane Horton and Sophia Huynh
-
 === Module description ===
 This module contains sample tests for Assignment 1.
 """
